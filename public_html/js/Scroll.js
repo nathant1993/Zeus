45a68435a2dcbb61ee29e270c7b2e8c012be6703
Dane Nutting <danenutting@connect.glos.ac.uk>
2015-05-19 19:42:37 +0100

    adding dynamic scroll js

--- public_html/js/Scroll.js
+++ public_html/js/Scroll.js
@@ -1,13 +1,32 @@
 /// <reference path="../../typings/jquery/jquery.d.ts"/>
 $(document).ready(function() {
+  
   $("#cloudicon").click(function scrolltofoot(){
+    
       $("#footer").velocity("scroll", { 
       duration: 500,
       delay: 0,
-      easing: "ease-out",
+      easing: "ease-in-out",
       mobileHA: false
       });
+      
   });
+  
+   // bind click event to all internal page anchors
+    $("a[href*=#]").bind("click", function(e) {
+        // prevent default action and bubbling
+        e.preventDefault();
+        e.stopPropagation();
+        // set target to anchor's "href" attribute
+        var target = $(this).attr("href");
+        // scroll to each target
+        $(target).velocity("scroll", {
+            duration: 500,
+            offset: -40,
+            easing: "ease-in-out"
+        });
+    });
+  
 });
 
 
