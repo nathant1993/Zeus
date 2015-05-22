ab0cc6f77a30f84797af671fe59979dbe9a8c82a
Dane Nutting <danenutting@connect.glos.ac.uk>
2015-05-22 14:43:58 +0100

    working on graphs

--- public_html/js/Scroll.js
+++ public_html/js/Scroll.js
@@ -1,6 +1,15 @@
-/// <reference path="../../typings/jquery/jquery.d.ts"/>
+/// <reference path="../js/jquery.d.ts"/>
+/*================================================================================================================*/
+/*================================================================================================================*/
+// JavaScript to scroll up and down to different divs on the page
+/*================================================================================================================*/
+/*================================================================================================================*/
+
 $(document).ready(function() {
   
+  var lastlink;
+  
+  //Testing scrolling
   $("#cloudicon").click(function scrolltofoot(){
     
       $("#footer").velocity("scroll", { 
@@ -19,15 +28,25 @@ $(document).ready(function() {
         e.stopPropagation();
         // set target to anchor's "href" attribute
         var target = $(this).attr("href");
+        //check if the last link is null - if it isn't remove the underline class 
+        //from the last link before it's applied to the next link unless navigating to the top of the page
+        if (!lastlink && target == "#hero"){}
+        else {
+            $(lastlink).removeClass("underline"); 
+           }
         // scroll to each target
         $(target).velocity("scroll", {
             duration: 500,
-            offset: 40,
+            offset: 0,
             easing: "ease-out"
-        });   
-        $(this).addClass("underline");
+        });
+        //Add the css class underline to the most recently 
+        //navigated to link unless navigating to the top of the page
+        if (target != "#hero") {
+          $(this).addClass("underline");
+          lastlink = $(this);
+        }   
+        
     });
   
 });
-
-
