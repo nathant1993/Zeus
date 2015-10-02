97307db2ee03a8fd1aaba5f272d122f8f1e574c9
Dane <dane.nutting@mac.com>
2015-10-02 12:28:34 +0100

    further validation of pbi form

--- public_html/js/deletePBI.js
+++ public_html/js/deletePBI.js
@@ -35,51 +35,75 @@
 		}
 		//If the ID isn't Null then submit the update
 	  	else{
-		//Ajax request to update the current PBI with whatever new value has been entered in to the form
-		$.ajax({
-			type: "POST",
-			url: "../php/DeletePBI.php",
-			data: {
-				postedID:updateID,
-			},
-			success: function(results) {
-				//style and add content to a status div that pops up to provide feedback on how the update went
-				$("#greyOut").velocity("transition.fadeIn")
-				.velocity({opacity:0.9});
-				$("#popupContact").velocity("transition.bounceUpIn")
-				.velocity({opacity:1});
-				$("#popupContact").prepend('<img id="msgImg" src="../images/tick.svg" /> <h1 id="msgH1">Your PBI was successfully deleted!</h1> <br> <a href="#" id="msgClose">Cancel</a>');
-				
-				//Close popup div and remove elements from the div so they don't stack up on each other
-				$("#msgClose").click(function(e) {
-					e.preventDefault();
-					$("#popupContact").velocity("transition.bounceDownOut");
-					$("#greyOut").velocity("transition.fadeOut",{delay:200});
-					$("#msgImg").remove();
-					$("#msgH1").remove();
-					$("#msgClose").remove();			
+			$("#greyOut").velocity("transition.fadeIn")
+			.velocity({opacity:0.9});
+			$("#popupContact").velocity("transition.bounceUpIn")
+			.velocity({opacity:1});
+			$("#popupContact").prepend('<img id="msgImg" src="../images/cross.svg" /> <h1 id="msgH1">Are you sure you want to delete this PBI?</h1> <br> <a href="#" id="confirmButton">Yes</a> <a href="#" id="msgClose">No</a>');
+			
+			$("#msgClose").click(function(e) {
+				e.preventDefault();
+				$("#popupContact").velocity("transition.bounceDownOut");
+				$("#greyOut").velocity("transition.fadeOut",{delay:200});
+				$("#msgImg").remove();
+				$("#msgH1").remove();
+				$("#msgClose").remove();
+				$("#confirmButton").remove();			
+			});
+			  
+			$("#confirmButton").click(function(e) {
+			e.preventDefault();
+			$("#msgImg").remove();
+			$("#msgH1").remove();
+			$("#msgClose").remove();
+			$("#confirmButton").remove();
+			
+				//Ajax request to update the current PBI with whatever new value has been entered in to the form
+				$.ajax({
+					type: "POST",
+					url: "../php/DeletePBI.php",
+					data: {
+						postedID:updateID,
+					},
+					success: function(results) {
+						//style and add content to a status div that pops up to provide feedback on how the update went
+						$("#greyOut").velocity("transition.fadeIn")
+						.velocity({opacity:0.9});
+						$("#popupContact").velocity("transition.bounceUpIn")
+						.velocity({opacity:1});
+						$("#popupContact").prepend('<img id="msgImg" src="../images/tick.svg" /> <h1 id="msgH1">Your PBI was successfully deleted!</h1> <br> <a href="#" id="msgClose">Cancel</a>');
+						
+						//Close popup div and remove elements from the div so they don't stack up on each other
+						$("#msgClose").click(function(e) {
+							e.preventDefault();
+							$("#popupContact").velocity("transition.bounceDownOut");
+							$("#greyOut").velocity("transition.fadeOut",{delay:200});
+							$("#msgImg").remove();
+							$("#msgH1").remove();
+							$("#msgClose").remove();			
+						});
+					},
+					error: function(results) {
+						//style and add content to a status div that pops up to provide feedback on how the update went
+						$("#greyOut").velocity("transition.fadeIn")
+						.velocity({opacity:0.9});
+						$("#popupContact").velocity("transition.bounceUpIn")
+						.velocity({opacity:1});
+						$("#popupContact").prepend('<img id="msgImg" src="../images/cross.svg" /> <h1 id="msgH1">Sorry we could not delete your PBI.</h1> <br> <a href="#" id="msgClose">Cancel</a>');
+						
+						//Close popup div and remove elements from the div so they don't stack up on each other$("#msgClose").click(function(e) {
+						$("#msgClose").click(function(e) {
+							e.preventDefault();
+							$("#popupContact").velocity("transition.bounceDownOut");
+							$("#greyOut").velocity("transition.fadeOut",{delay:200});
+							$("#msgImg").remove();
+							$("#msgH1").remove();
+							$("#msgClose").remove();			
+						});
+					}
 				});
-			},
-			error: function(results) {
-				//style and add content to a status div that pops up to provide feedback on how the update went
-				$("#greyOut").velocity("transition.fadeIn")
-				.velocity({opacity:0.9});
-				$("#popupContact").velocity("transition.bounceUpIn")
-				.velocity({opacity:1});
-				$("#popupContact").prepend('<img id="msgImg" src="../images/cross.svg" /> <h1 id="msgH1">Sorry we could not delete your PBI.</h1> <br> <a href="#" id="msgClose">Cancel</a>');
-				
-				//Close popup div and remove elements from the div so they don't stack up on each other$("#msgClose").click(function(e) {
-				$("#msgClose").click(function(e) {
-					e.preventDefault();
-					$("#popupContact").velocity("transition.bounceDownOut");
-					$("#greyOut").velocity("transition.fadeOut",{delay:200});
-					$("#msgImg").remove();
-					$("#msgH1").remove();
-					$("#msgClose").remove();			
-				});
-			}
-		});
-	  }
+			});
+	  	}
 	});
 	return false;
  });
--- public_html/js/updatePBIs.js
+++ public_html/js/updatePBIs.js
@@ -27,7 +27,7 @@
 			.velocity({opacity:0.9});
 			$("#popupContact").velocity("transition.bounceUpIn")
 			.velocity({opacity:1});
-			$("#popupContact").prepend('<img id="msgImg" src="../images/cross.svg" /> <h1 id="msgH1">Sorry we could not update your PBI.</h1> <br> <a href="#" id="msgClose">Cancel</a>');
+			$("#popupContact").prepend('<img id="msgImg" src="../images/cross.svg" /> <h1 id="msgH1">Sorry we could not update your PBI.</h1> <br> <a href="#" id="msgClose">OK</a>');
 			
 			//Close popup div and remove elements from the div so they don't stack up on each other
 			$("#msgClose").click(function(e) {
@@ -41,59 +41,82 @@
 		  return false;
 	  }
 	  //If the ID isn't Null then submit the update
-	  else{
-		//Ajax request to update the current PBI with whatever new value has been entered in to the form
-		$.ajax({
-			type: "POST",
-			url: "../php/UpdatePBI.php",
-			data: {
-				postedID:updateID,
-				postedTitle:updateTitle,
-				postedDesc:updateDesc,
-				postedEffort:updateEffort,
-				postedPriority:updatePriority,
-				postedState:updateState,
-				postedIteration:updateIteration,
-				postedProject:updateProject
-			},
-			success: function(results) {
-				//style and add content to a status div that pops up to provide feedback on how the update went
-				$("#greyOut").velocity("transition.fadeIn")
-				.velocity({opacity:0.9});
-				$("#popupContact").velocity("transition.bounceUpIn")
-				.velocity({opacity:1});
-				$("#popupContact").prepend('<img id="msgImg" src="../images/tick.svg" /> <h1 id="msgH1">Your PBI was successfully updated!</h1> <br> <a href="#" id="msgClose">Cancel</a>');
+	  else{  
+			$("#greyOut").velocity("transition.fadeIn")
+			.velocity({opacity:0.9});
+			$("#popupContact").velocity("transition.bounceUpIn")
+			.velocity({opacity:1});
+			$("#popupContact").prepend('<img id="msgImg" src="../images/cross.svg" /> <h1 id="msgH1">Are you sure you want to update this PBI?</h1> <br> <a href="#" id="confirmButton">Yes</a> <a href="#" id="msgClose">No</a>');
+			
+			$("#msgClose").click(function(e) {
+				e.preventDefault();
+				$("#popupContact").velocity("transition.bounceDownOut");
+				$("#greyOut").velocity("transition.fadeOut",{delay:200});
+				$("#msgImg").remove();
+				$("#msgH1").remove();
+				$("#msgClose").remove();
+				$("#confirmButton").remove();			
+			});
 				
-				//Close popup div and remove elements from the div so they don't stack up on each other
-				$("#msgClose").click(function(e) {
-					e.preventDefault();
-					$("#popupContact").velocity("transition.bounceDownOut");
-					$("#greyOut").velocity("transition.fadeOut",{delay:200});
-					$("#msgImg").remove();
-					$("#msgH1").remove();
-					$("#msgClose").remove();			
+		  	$("#confirmButton").click(function(e) {
+				e.preventDefault();
+				$("#msgImg").remove();
+				$("#msgH1").remove();
+				$("#msgClose").remove();
+				$("#confirmButton").remove();
+				//Ajax request to update the current PBI with whatever new value has been entered in to the form
+				$.ajax({
+					type: "POST",
+					url: "../php/UpdatePBI.php",
+					data: {
+						postedID:updateID,
+						postedTitle:updateTitle,
+						postedDesc:updateDesc,
+						postedEffort:updateEffort,
+						postedPriority:updatePriority,
+						postedState:updateState,
+						postedIteration:updateIteration,
+						postedProject:updateProject
+					},
+					success: function(results) {
+						//style and add content to a status div that pops up to provide feedback on how the update went
+						$("#greyOut").velocity("transition.fadeIn")
+						.velocity({opacity:0.9});
+						$("#popupContact").velocity("transition.bounceUpIn")
+						.velocity({opacity:1});
+						$("#popupContact").prepend('<img id="msgImg" src="../images/tick.svg" /> <h1 id="msgH1">Your PBI was successfully updated!</h1> <br> <a href="#" id="msgClose">OK</a>');
+						
+						//Close popup div and remove elements from the div so they don't stack up on each other
+						$("#msgClose").click(function(e) {
+							e.preventDefault();
+							$("#popupContact").velocity("transition.bounceDownOut");
+							$("#greyOut").velocity("transition.fadeOut",{delay:200});
+							$("#msgImg").remove();
+							$("#msgH1").remove();
+							$("#msgClose").remove();			
+						});
+					},
+					error: function(results) {
+						//style and add content to a status div that pops up to provide feedback on how the update went
+						$("#greyOut").velocity("transition.fadeIn")
+						.velocity({opacity:0.9});
+						$("#popupContact").velocity("transition.bounceUpIn")
+						.velocity({opacity:1});
+						$("#popupContact").prepend('<img id="msgImg" src="../images/cross.svg" /> <h1 id="msgH1">Sorry we could not update your PBI.</h1> <br> <a href="#" id="msgClose">OK</a>');
+						
+						//Close popup div and remove elements from the div so they don't stack up on each other$("#msgClose").click(function(e) {
+						$("#msgClose").click(function(e) {
+							e.preventDefault();
+							$("#popupContact").velocity("transition.bounceDownOut");
+							$("#greyOut").velocity("transition.fadeOut",{delay:200});
+							$("#msgImg").remove();
+							$("#msgH1").remove();
+							$("#msgClose").remove();			
+						});
+					}
 				});
-			},
-			error: function(results) {
-				//style and add content to a status div that pops up to provide feedback on how the update went
-				$("#greyOut").velocity("transition.fadeIn")
-				.velocity({opacity:0.9});
-				$("#popupContact").velocity("transition.bounceUpIn")
-				.velocity({opacity:1});
-				$("#popupContact").prepend('<img id="msgImg" src="../images/cross.svg" /> <h1 id="msgH1">Sorry we could not update your PBI.</h1> <br> <a href="#" id="msgClose">Cancel</a>');
-				
-				//Close popup div and remove elements from the div so they don't stack up on each other$("#msgClose").click(function(e) {
-				$("#msgClose").click(function(e) {
-					e.preventDefault();
-					$("#popupContact").velocity("transition.bounceDownOut");
-					$("#greyOut").velocity("transition.fadeOut",{delay:200});
-					$("#msgImg").remove();
-					$("#msgH1").remove();
-					$("#msgClose").remove();			
-				});
-			}
-		});
-	  }
+			});
+	  	}
 	});
 	return false;
  });
