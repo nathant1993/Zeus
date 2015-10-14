f53ba65e24e8adc294518927797341cd2c0b85d8
Dane Nutting <danenutting@connect.glos.ac.uk>
2015-10-14 11:31:58 +0100

    Added JS validation to password rest

--- public_html/js/resetPassword.js
+++ public_html/js/resetPassword.js
@@ -10,6 +10,7 @@ $(function () {
 			var form = document.getElementById('loginForm');
 			var status = document.getElementById("success");
 			
+			//check if the password and confirm password boxes contain the same thing
 			if($(password).val() != $(confirmPassword).val()){
 				
 				status.innerHTML = "Your passwords don't match";
@@ -20,15 +21,47 @@ $(function () {
 				  
 				return false;
 			}
-			// else if ($(password).text.length < 7) {
-			// 	status.innerHTML = "Your password is less than 7 characters long";
-			// 	status.style.opacity = 1;
-			// 	status.style.color = "#ff6700";
-			// 	$("#loginForm").velocity("callout.shake");
-		  	// 	$("#success").velocity("callout.shake");
+			//check if passwords contain a number
+			else if(!$(password).val().match(/([\d])/)){
+				status.innerHTML = "Your password needs to contain a number"
+				status.style.opacity = 1;
+				status.style.color = "#ff6700";
+				$("#loginForm").velocity("callout.shake");
+		  		$("#success").velocity("callout.shake");
 				  
-			// 	return false;
-			// } 
+				return false;
+			}
+			//check if passwords contain a lower case letter
+			else if(!$(password).val().match(/([a-z])/)){
+				status.innerHTML = "Your password needs to contain a lower case letter"
+				status.style.opacity = 1;
+				status.style.color = "#ff6700";
+				$("#loginForm").velocity("callout.shake");
+		  		$("#success").velocity("callout.shake");
+				  
+				return false;
+			}
+			//check if passwords contain an uppercase letter
+			else if(!$(password).val().match(/([A-Z])/)){
+				status.innerHTML = "Your password needs to contain an upper case letter"
+				status.style.opacity = 1;
+				status.style.color = "#ff6700";
+				$("#loginForm").velocity("callout.shake");
+		  		$("#success").velocity("callout.shake");
+				  
+				return false;
+			}
+			//check if a password is it characters long or not
+			else if ($(password).val().length < 8) {
+				status.innerHTML = "Your password needs to contain 8 characters or more";
+				status.style.opacity = 1;
+				status.style.color = "#ff6700";
+				$("#loginForm").velocity("callout.shake");
+		  		$("#success").velocity("callout.shake");
+				  
+				return false;
+			} 
+			//If the passwords pass these checks then submit the password submit form
 			else{
 				form.submit();
 				return true;
