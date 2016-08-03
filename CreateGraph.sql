4a666a1e882861595c70fb681516cef0ff331f7a
Dane <DaneNutting@users.noreply.github.com>
2016-08-03 07:42:28 +0100

    Delete CreateGraph.sql

--- CreateGraph.sql
+++
@@ -1,7 +0,0 @@
-SELECT b.iteration_id, iteration_name, SUM( PBI_effort ) as 'effort', starting_effort, CalcEffRemaining(b.iteration_id) as 'effort_done_to_date', (starting_effort - CalcEffRemaining(b.iteration_id)) as 'remaining_effort'
-	FROM  backlog_items a
-	right outer join iteration b on b.iteration_ID = a.iteration_ID
-    where b.iteration_start_date <= sysdate()
-    and a.state_id = 4
-	GROUP BY iteration_id, iteration_name, CalcEffRemaining(a.iteration_id), (starting_effort - CalcEffRemaining(a.iteration_id))
-    
